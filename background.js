fd0a7499afe729ddfc0f7c39e39a8f883031a6a2
cedylad <cedricanassi@gmail.com>
2023-08-10 14:41:23 +0200

    design

--- background.js
+++ background.js
@@ -4,10 +4,3 @@ chrome.action.onClicked.addListener((tab) => {
         function: generatePassword
     });
 });
-
-function generatePassword() {
-    const len_password = parseInt(prompt("Password length:"));
-    const character = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@[]#$())*+.:;{}|~?";
-    const pswd = Array.from({ length: len_password }, () => character[Math.floor(Math.random() * character.length)]).join(""); // Generate the password
-    alert("Generated password: " + pswd);
-}
